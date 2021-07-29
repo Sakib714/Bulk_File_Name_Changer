67941b13753f3bfed76a624b230acfb3aad0b1f7
Sakib714 <sakib39.official@gmail.com>
2021-07-30 02:12:55 +0600

    Added Comments

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+# Developed by Sadman Sakib
 import os
 
 
-path = 'C:/Users/N.S Computer/OneDrive/Desktop/temp/'
+path = 'C:/Users/N.S Computer/OneDrive/Desktop/temp/'   # Path Directory
 i=1
 for file in os.listdir(path):
-    new_name = 'SSS'+str(i)
-    pre_file_name = path + file
-    new_file_name = path + new_name
-    os.rename(pre_file_name, new_file_name)
-    i += 1
+    new_name = 'SSS'+str(i)                 # Setting new unique name
+    pre_file_name = path + file             # Obj with pre. fila name % directory
+    new_file_name = path + new_name         # Obj with new. fila name % directory
+    os.rename(pre_file_name, new_file_name) # Renaming Files
+    i += 1                                  # Incrementing Value of i
 
 
 
